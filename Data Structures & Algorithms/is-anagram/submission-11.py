9302ae3342fd437c2d43956174d7b78bc91e504a
Khushi <123492187+khushi-kh@users.noreply.github.com>
2026-05-08 17:10:24 +0530

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t): return False
+        
+        freq_s = {}
+        freq_t = {}
+
+        for i in range(len(s)):
+
+            if s[i] in freq_s:
+                freq_s[s[i]] += 1
+            else:
+                freq_s[s[i]] = 1
+
+            if t[i] in freq_t:
+                freq_t[t[i]] += 1
+            else:
+                freq_t[t[i]] = 1
+
+        return freq_s == freq_t
