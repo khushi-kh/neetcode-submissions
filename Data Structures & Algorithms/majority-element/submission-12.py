59dcc33638d66522e679b68089a3db8730171d73
Khushi <123492187+khushi-kh@users.noreply.github.com>
2026-05-13 22:24:00 +0530

    Add: majority-element - submission-12

---
+++ Data Structures & Algorithms/majority-element/submission-12.py
@@ -0,0 +1,13 @@
+import random
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        for num in nums:
+            if nums.count(num) > n//2:
+                return num
+
+
+        
